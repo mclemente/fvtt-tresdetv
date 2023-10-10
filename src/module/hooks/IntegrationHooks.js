763d16b5735e6f1eace35e39fbf9509a1bcea02c
Matheus Clemente <matheus.xb@gmail.com>
2023-10-10 16:38:35 -0300

    Add dice tray support

--- src/module/hooks/IntegrationHooks.js
+++ src/module/hooks/IntegrationHooks.js
@@ -1,1 +1,35 @@
-export default class IntegrationHooks {}
+export default class IntegrationHooks {
+	static dcCalcWhitelist(whitelist, actor) {
+		// Add whitelist support for the calculator.
+		whitelist.tresdetv = {
+			custom: {
+				custom: {
+					"1d6": {
+						label: "1d6",
+						name: "1D",
+						formula: "1d6",
+					},
+					"2d6": {
+						label: "2d6",
+						name: "2D",
+						formula: "2d6",
+					},
+					"3d6": {
+						label: "3d6",
+						name: "3D",
+						formula: "3d6",
+					},
+				},
+			},
+		};
+
+		// Add a custom row of buttons based on 13th Age's ability damage at high levels.
+		for (let prop in actor.system.atributos) {
+			whitelist.tresdetv.custom.custom[prop] = {
+				label: prop,
+				name: prop.capitalize(),
+				formula: `@${prop}`,
+			};
+		}
+	}
+}
