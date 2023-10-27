91f613eb2445258d692a3a45a465966c134e6fb7
Matheus Clemente <matheus.xb@gmail.com>
2023-10-26 21:15:27 -0300

    Update IntegrationHooks.js

--- src/module/hooks/IntegrationHooks.js
+++ src/module/hooks/IntegrationHooks.js
@@ -1,36 +1,19 @@
 export default class IntegrationHooks {
 	// Dice Tray module
-	static dcCalcWhitelist(whitelist, actor) {
-		// Add whitelist support for the calculator.
-		whitelist.tresdetv = {
-			custom: {
-				custom: {
-					"1d6": {
-						label: "1d6",
-						name: "1D",
-						formula: "1d6",
+	static "dice-calculator.keymaps"(diceMaps, Template) {
+		class TresDeTV extends Template {
+			get dice() {
+				return [
+					{
+						d6: { label: "1D" },
+						"2d6": { label: "2D" },
+						"3d6": { label: "3D" },
 					},
-					"2d6": {
-						label: "2d6",
-						name: "2D",
-						formula: "2d6",
-					},
-					"3d6": {
-						label: "3d6",
-						name: "3D",
-						formula: "3d6",
-					},
-				},
-			},
-		};
+				];
+			}
 
-		// Add a custom row of buttons based on 13th Age's ability damage at high levels.
-		for (let prop in actor.system.atributos) {
-			whitelist.tresdetv.custom.custom[prop] = {
-				label: prop,
-				name: prop.capitalize(),
-				formula: `@${prop}`,
-			};
+			// TODO override updateChatDice to change flag_button
 		}
+		diceMaps.tresdetv = TresDeTV;
 	}
 }
