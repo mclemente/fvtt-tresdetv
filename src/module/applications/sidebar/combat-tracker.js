3d75aec9f268c6984c29061629090f45b5c8989d
Matheus Clemente <matheus.xb@gmail.com>
2026-07-24 16:49:29 -0300

    Update combat-tracker.js

--- src/module/applications/sidebar/combat-tracker.js
+++ src/module/applications/sidebar/combat-tracker.js
@@ -1,9 +1,10 @@
 export default class CombatTrackerTresDeTV extends foundry.applications.sidebar.tabs.CombatTracker {
-	async _onCombatantControl(event) {
-		const btn = event.currentTarget;
+	async _onCombatantControl(event, target) {
+		const btn = target || event.currentTarget;
 		const combatantId = btn.closest(".combatant").dataset.combatantId;
 		const combatant = this.viewed.combatants.get(combatantId);
-		if (btn.dataset.control === "rollInitiative" && combatant?.actor) return combatant.actor.rollInitiativeDialog();
+		const action = btn.dataset.control || btn.dataset.action;
+		if (action === "rollInitiative" && combatant?.actor) return combatant.actor.rollInitiativeDialog();
 		return super._onCombatantControl(event);
 	}
 }
